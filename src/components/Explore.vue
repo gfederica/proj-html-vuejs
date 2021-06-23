a6a5b52c327fcdc7454ad637abdcc073f567c072
gfederica <effegiudice@gmail.com>
2021-06-23 16:38:42 +0200

    finita sezione explore

--- src/components/Explore.vue
+++ src/components/Explore.vue
@@ -1,4 +1,6 @@
 <template>
+
+<div class="explore-all">
   <section class="explore">
       <div class="container-60">
           <h2 class="text-center">Let's Dream Big Together</h2>
@@ -42,6 +44,50 @@
           </div>
         </div>
     </section>
+    <section class="explore-white-bg">
+        <div class="container-85">
+            <h2 class="text-center">Let's Dream Big Together</h2>
+            <h1 class="text-center">Explore How can I help you</h1>
+            <div class="features nowrap">
+                <div class="card hero text-white rounded">
+                    <img src="../assets/course-02-443x600.jpg" class="card-img" alt="background">
+                    <div class="hero-card"></div>
+                    <div class="card-img-overlay">
+                        <h2 class="card-title">$40.<span class="smaller">00</span></h2>
+                        <h2 class="card-text">Learning to Write as a Professional Author</h2>
+                    </div>
+                </div>
+                <div class="card hero text-white rounded">
+                    <img src="../assets/stock-full-hd-03-443x600.jpg" class="card-img" alt="background">
+                    <div class="hero-card"></div>
+                    <div class="card-img-overlay">
+                        <h2 class="card-title">Free</h2>
+                        <h2 class="card-text">Customer-centric Info-Tech Strategies</h2>
+                    </div>
+                </div>
+                <div class="card hero text-white rounded">
+                    <img src="../assets/stock-full-hd-04-443x600.jpg" class="card-img" alt="background">
+                    <div class="hero-card"></div>
+                    <div class="card-img-overlay">
+                        <h2 class="card-title">$19.<span class="smaller">00</span></h2>
+                        <h2 class="card-text">Open Programming Courses for Everyone: Python</h2>
+                    </div>
+                </div>
+                <div class="card hero text-white rounded">
+                    <img src="../assets/stock-full-hd-06-443x600.jpg" class="card-img" alt="background">
+                    <div class="hero-card"></div>
+                    <div class="card-img-overlay">
+                        <h2 class="card-title">$26.<span class="smaller">00</span></h2>
+                        <h2 class="card-text">Academic Listening and Note-taking</h2>
+                    </div>
+                </div>
+            </div>
+            <div class="view-all-courses">
+                <p class="text-center">Take the control of their life back and start doing things to make their dream come true. <span><a href="">View all courses <i class="fas fa-arrow-right"></i></a></span></p>
+            </div>
+        </div>
+    </section>
+</div>
 </template>
 
 <script>
@@ -54,22 +100,30 @@ export default {
 @import '../style/general.scss';
 @import '../style/mixins.scss';
 
-.explore {
-    padding: 100px 0;
-    background-color: $exploreGrey;
-
-        h2 {
-            font-size: $h2;
-            color: $mainGreen;
-        }
-
-        h1 {
-            font-size: $h1;
-            color: $mainText;
-            font-weight: 600;
-        }
+h2 {
+    font-size: $h2;
+    color: $mainGreen;
+}
+h1 {
+    font-size: $h1;
+    color: $mainText;
+    font-weight: 600;
 }
 
+
+.explore,
+.explore-white-bg {
+    padding: 100px 0;
+}
+
+.explore {
+    background-color: $exploreGrey;
+}
+.explore-white-bg {
+    background-color: $white;
+}
+
+// prima sezione
 .features {
     display: flex;
     flex-wrap: wrap;
@@ -118,6 +172,10 @@ export default {
             width: 100%;
             border-top: 1px solid $jumbotron;
         }
+
+        .hero {
+            width: 25%;
+        }
 }
 
 .slider {
@@ -131,6 +189,62 @@ export default {
        &:nth-child(1) {
             color: $mainText;
             font-size: $smallText;
+        }
+    }
+}
+
+
+// seconda sezione
+// overlay
+.hero-card::after {
+    content: "";
+    position: absolute;
+    top:0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: linear-gradient(to bottom, transparent 60%, black 100%);
+    background-blend-mode: overlay;
+}
+
+.nowrap {
+    flex-wrap: nowrap;
+}
+
+.card-img-overlay {
+    top: 68%;
+
+    h2:not(.card-text) {
+        font-size: $h2;
+        color: $mainGreen;
+        font-weight: 700;
+    }
+
+    h2 {
+        color: $white;
+    }
+
+    .smaller {
+        font-size: $h5;
+    }
+}
+
+.view-all-courses {
+    width: 35%;
+    margin: auto;
+    padding-top: 30px;
+
+    p {
+        font-size: $mediumSmallText;
+        font-weight: 600;
+        color: $mainGrey;
+        line-height: 24px;
+
+        a {
+           color: $mainText;
+           font-weight: 700;
+           text-underline-offset: 4px;
+           text-decoration-color: $silver;
         }
     }
 }
